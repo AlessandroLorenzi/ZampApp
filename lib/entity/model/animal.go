5eddb35b004764803b40608311407a1335e5d79d
Alessandro Lorenzi <alorenzi@alorenzi.eu>
2020-10-18 19:24:36 +0200

    separated animal and location
    Refs:

--- lib/entity/model/animal.go
+++ lib/entity/model/animal.go
@@ -1,19 +1,7 @@
 package model
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/google/uuid"
-
-	"github.com/sirupsen/logrus"
-
-	"github.com/paulmach/orb"
-
-	"github.com/paulmach/orb/encoding/wkb"
-
-	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 type Animal struct {
@@ -22,7 +10,7 @@ type Animal struct {
 	Breed         string   `gorm:"NOT NULL" json:"breed"`
 	Size          int      `gorm:"NOT NULL" json:"size"`
 	Sex           bool     `gorm:"NOT NULL" json:"sex"`
-	OwnerID       int      `gorm:"NOT NULL" json:"owner_id"`
+	OwnerID       string   `gorm:"NOT NULL" json:"owner_id"`
 	Owner         User     `gorm:"foreignKey:OwnerID" json:"owner" `
 	Picture       string   `gorm:"NOT NULL" json:"picture"`
 	Wormed        bool     `gorm:"NOT NULL" json:"wormed"`
@@ -59,47 +47,4 @@ func NewAnimal(
 		PositionDesc:  PositionDesc,
 		Description:   Description,
 	}, nil
-}
-
-type Location struct {
-	X float64 `json:"x"`
-	Y float64 `json:"y"`
-}
-
-// Scan implements the sql.Scanner interface
-func (loc *Location) Scan(v interface{}) error {
-	// Scan a value into struct from database driver
-	//err := json.Unmarshal(v, loc)
-	if v == nil {
-		return nil
-	}
-
-	mysqlEncoding, ok := v.([]byte)
-	if !ok {
-		logrus.Errorf("did not scan: expected []byte but was %T", v)
-		return fmt.Errorf("did not scan: expected []byte but was %T", v)
-	}
-
-	var g wkb.GeometryScanner
-	err := g.Scan(mysqlEncoding[4:])
-	if !ok {
-		logrus.Errorf("did not scan: %v", err)
-		return fmt.Errorf("did not scan: %w", err)
-	}
-
-	loc.X = g.Geometry.(orb.Point).X()
-	loc.Y = g.Geometry.(orb.Point).Y()
-
-	return err
-}
-
-func (loc Location) GormDataType() string {
-	return "geometry"
-}
-
-func (loc Location) GormValue(ctx context.Context, db *gorm.DB) clause.Expr {
-	return clause.Expr{
-		SQL:  "ST_PointFromText(?, 4326)",
-		Vars: []interface{}{fmt.Sprintf("POINT(%f %f)", loc.X, loc.Y)},
-	}
 }
